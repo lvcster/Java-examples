639bcba8a22c785c5ed63b70238a9fc7fd3cf536
lvcster <lvcster@gmail.com>
2019-05-22 15:32:28 +0200

    Add files via upload

---
+++ SpecialCharsUtil.java
@@ -0,0 +1,21 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+ 
+public class SpecialCharsUtil {
+ 
+   public static void main(String[] args) {
+ 
+      String str = "abc$def^ghi#jkl";
+ 
+      Pattern p = Pattern.compile("[^a-z0-9 ]", Pattern.CASE_INSENSITIVE);
+      Matcher m = p.matcher(str);
+ 
+      System.out.println(str);
+      int count = 0;
+      while (m.find()) {
+         count = count+1;
+         System.out.println("position "  + m.start() + ": " + str.charAt(m.start()));
+      }
+      System.out.println("There are " + count + " special characters");
+   }
+}
